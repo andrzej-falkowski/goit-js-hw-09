f31b32799cd0f5f2d5c947b63a5cc0735c3d9cbc
andrzej-falkowski <andrzejfalkowski88@gmail.com>
2023-11-24 20:50:54 +0100

    Added first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let backgroundInterval;
+
+function setColor() {
+  document.body.style.background = getRandomHexColor();
+}
+
+startBtn.addEventListener('click', event => {
+  event.target.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+  backgroundInterval = setInterval(setColor, 1000);
+});
+
+stopBtn.addEventListener('click', (event) => {
+    event.target.setAttribute('disabled', '');
+    startBtn.removeAttribute('disabled');
+  clearInterval(backgroundInterval);
+});
